fc10ff5032f3d303391b3ca572d2a71fe086ca1a
rheabeskia <rhea.gofton@gmail.com>
2023-09-14 13:23:00 +1000

    Added code to populate Python lists with csv values

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+# load the data from the csv file
+cd = pd.read_csv("climate.csv")
+
+# add it to the lists
+years = cd['Year']
+# print(years)
+co2 = cd['CO2']
+temp = cd['Temperature']
+
+# show it in a figure
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
 
